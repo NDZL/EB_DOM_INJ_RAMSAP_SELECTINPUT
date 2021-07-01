18aa7a7b69ff1e2f1c71c81a1032112ba3731ad2
{ N.DZL } <NDZL@users.noreply.github.com>
2021-07-01 11:56:24 +0200

    highlighting First Input only
    working to get a generic solution
    removed page filter
    working only for 1st input field
    source code of new pages yet to be seen

---
+++ highlightFirstInput.js
@@ -0,0 +1,4 @@
+var input1st = document.querySelectorAll('input[type=text]')[0].id
+document.getElementById( input1st ).focus();
+document.getElementById( input1st ).select();
+document.getElementById( input1st ).autofocus = true;
